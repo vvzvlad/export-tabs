3975466916533a9293c0fd29d79f7517c047c6dd
vvzvlad <vvzvlad@gmail.com>
2021-01-27 03:29:49 +0300

    Update exptabs.js

--- exptabs.js
+++ exptabs.js
@@ -1,102 +1,77 @@
-    var targetWindow = null;
-    var tabCount = 0;
+var targetWindow = null;
+var tabCount = 0;
 
-    function start(tab) {
-      chrome.windows.getCurrent(getWindows);
-    }
+function start(tab) {
+  chrome.windows.getCurrent(getWindows);
+}
 
-    function getWindows(win) {
-      targetWindow = win;
-      chrome.tabs.getAllInWindow(targetWindow.id, getTabs);
-    }
+function getWindows(win) {
+  targetWindow = win;
+  chrome.tabs.getAllInWindow(targetWindow.id, getTabs);
+}
 
-    function getTabs(tabs) {
-      tabCount = tabs.length;
-      chrome.windows.getAll({"populate" : true}, expTabs);
-    }
+function getTabs(tabs) {
+  tabCount = tabs.length;
+  chrome.windows.getAll({ "populate": true }, expTabs);
+}
 
-    function expTabs(windows) {
-      var numWindows = windows.length;
-    var exportAll = document.getElementById('inclAll').checked == true ? 1 : 0;
-    document.getElementById('content').value = '';
-      for (var i = 0; i < numWindows; i++) {
-        var win = windows[i];
-        if (targetWindow.id == win.id || exportAll == 1) {
-          var numTabs = win.tabs.length;
-          for (var j = 0; j < numTabs; j++) {
-            var tab = win.tabs[j];
-			if (document.getElementById('inclTitle').checked == true) {
-				document.getElementById('content').value += tab.title + '\n';
-				}
-			document.getElementById('content').value += tab.url + '\n\n';
-          }
-        }
+function expTabs(windows) {
+  var numWindows = windows.length;
+  document.getElementById('content').value = '';
+  for (var i = 0; i < numWindows; i++) {
+    var win = windows[i];
+    if (targetWindow.id == win.id) {
+      var numTabs = win.tabs.length;
+      for (var j = 0; j < numTabs; j++) {
+        var tab = win.tabs[j];
+        document.getElementById('content').value += tab.url + '\n';
       }
     }
+  }
+  document.getElementById('content').value = document.getElementById('content').value.split(/\s/g).filter((word, i, arr) => arr.indexOf(word) === i).sort().join('\n');
+}
 
-  function openTabs() {
-    var content = document.getElementById('content').value;
-    var rExp = new RegExp(
-        "(^|[ \t\r\n])((ftp|http|https|news|file|view-source|chrome):(([A-Za-z0-9$_.+!*(),;/?:@&~=-])|%[A-Fa-f0-9]{2}){2,}(#([a-zA-Z0-9][a-zA-Z0-9$_.+!*(),;/?:@&~=%-]*))?([A-Za-z0-9$_+!*();/?:~-])*)"
-        ,"g"
-      );
-    var newTabs = content.match(rExp);
-    if (newTabs != null) {
-      var newTabsLen = newTabs.length;
-      for (var j = 0; j < newTabsLen; j++) {
-        var nt = newTabs[j];
-        chrome.tabs.create({url: nt, active: false });
-      }
-    } else {
-      alert('Only fully qualified URLs will be opened.');
+function openTabs() {
+  var content = document.getElementById('content').value;
+  var rExp = new RegExp(
+    "(^|[ \t\r\n])((ftp|http|https|news|file|view-source|chrome):(([A-Za-z0-9$_.+!*(),;/?:@&~=-])|%[A-Fa-f0-9]{2}){2,}(#([a-zA-Z0-9][a-zA-Z0-9$_.+!*(),;/?:@&~=%-]*))?([A-Za-z0-9$_+!*();/?:~-])*)"
+    , "g"
+  );
+  var newTabs = content.match(rExp);
+  if (newTabs != null) {
+    for (var j = 0; j < newTabs.length; j++) {
+      var nt = newTabs[j];
+      chrome.tabs.create({ url: nt, active: false });
     }
+  } else {
+    alert('Only fully qualified URLs will be opened.');
   }
+}
 
-  function sendMail(gm) {
-    var action_url = "mailto:?";
-    //action_url += "subject=" + encodeURIComponent(subject) + "&";
-    action_url += "body=" + encodeURIComponent(document.getElementById('content').value);
-    if (gm == 1) {
-      var custom_url = "https://mail.google.com/mail/?extsrc=mailto&url=%s";
-      action_url = custom_url.replace("%s", encodeURIComponent(action_url));
-      chrome.tabs.create({ url: action_url });
-    } else {
-      chrome.tabs.update(tab_id, { url: action_url });
-    }
-  }
-  
-  function download() {
-	
-    
-	var content = document.getElementById('content').value
-	var content_arr = content.split('\n\n');
-	var data = '<html><head></head><body>';
-	for (var i = 0; i < content_arr.length; i++) {
-		var content_url = content_arr[i].split('\n');
-		if (document.getElementById('inclTitle').checked == true) {
-			data+='<a href="'+content_url[1]+'">'+content_url[0]+'</a><br/>';
-			} else {
-			data+='<a href="'+content_arr[i]+'">'+content_arr[i]+'</a><br/>';
-			}
-		}
-	data+='</body></html>';
+function copy_urls() {
+  navigator.clipboard.writeText(document.getElementById('content').value).then(function () {
+    console.log('Async: Copying to clipboard was successful!');
+  }, function (err) {
+    console.error('Async: Could not copy text: ', err);
+  });
 
-	var blob = new Blob([data], {type: "text/html;charset=utf-8"});
-	var url = URL.createObjectURL(blob);
-	var a = document.createElement('a');
-	
-	a.download = "tabs.html";
-	a.href = url;
-	a.click();
+}
 
-  }
+function paste_and_open() {
+  navigator.clipboard.readText()
+    .then(text => {
+      document.getElementById('content').value = text;
+    })
+    .catch(err => {
+      console.error('Failed to read clipboard contents: ', err);
+    });
+  openTabs();
+}
+
 
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('#btOpenTabs').addEventListener('click', openTabs);
-  document.querySelector('#inclTitle').addEventListener('click', start);
-  document.querySelector('#inclAll').addEventListener('click', start);
-  document.querySelector('#sendMail0').addEventListener('click', function(){sendMail(0)});
-  document.querySelector('#sendMail1').addEventListener('click', function(){sendMail(1)});
-  document.querySelector('#download').addEventListener('click', download);
+  document.querySelector('#paste_and_open').addEventListener('click', paste_and_open);
+  document.querySelector('#copy_urls').addEventListener('click', copy_urls);
   start();
 });
